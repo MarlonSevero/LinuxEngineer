3d1c77dcc109c9be6256551da0d8d2af8e2d7d04
Marlon Severo <marlonsallessevero@hotmail.com>
2025-04-24 18:25:31 -0300

    Bind9 chroot config-v2

--- bind9-chroot-dns-service.sh
+++ bind9-chroot-dns-service.sh
@@ -29,14 +29,30 @@ mv /etc/bind $BIND_DIR
 echo "[+] Criando link simbólico de /etc/bind para chroot..."
 ln -s $BIND_DIR /etc/bind
 
-echo "[+] Copiando localtime para o chroot..."
-cp /etc/localtime $CHROOT_DIR/etc/
-
 echo "[+] Corrigindo permissões..."
 chown -R $BIND_USER:$BIND_USER $CHROOT_DIR
+chown bind:bind $BIND_DIR/rndc.key
+chown bind:bind $BIND_CACHE -R
+chmod 775 $BIND_CACHE -R
 
-echo "[+] Liberando AppArmor para o BIND..."
-aa-complain /etc/apparmor.d/usr.sbin.named
+echo "[+] Configurando AppArmor para permitir os novos dispositivos e chroot..."
+
+# Adicionando permissões no perfil do AppArmor a partir da linha 25
+echo "[+] Adicionando permissões no AppArmor..."
+
+# Inserindo permissões específicas no /etc/apparmor.d/usr.sbin.named a partir da linha 25
+awk 'NR==25 {print "# Permissões para o chroot do BIND9\n/var/named/chroot/** r,\n/var/named/chroot/var/cache/bind/** rw,\n/var/named/chroot/etc/bind/** r,\n/var/named/chroot/dev/null rw,\n/var/named/chroot/dev/random rw,"}1' /etc/apparmor.d/usr.sbin.named > /tmp/named.apparmor && mv /tmp/named.apparmor /etc/apparmor.d/usr.sbin.named
+
+# Recarregando o perfil do AppArmor para aplicar as novas permissões
+echo "[+] Recarregando AppArmor..."
+apparmor_parser -r /etc/apparmor.d/usr.sbin.named
+
+echo "[+] Copiando /etc/localtime para o chroot..."
+cp /etc/localtime $CHROOT_DIR/etc/
+
+# Alterando o PIDFILE no arquivo de init do named
+echo "[+] Alterando PIDFILE no script de init..."
+sed -i 's|PIDFILE=/var/run/named/named.pid|PIDFILE=/var/bind9/chroot/var/run/named/named.pid|' /etc/init.d/bind9
 
 echo "[+] Configuração concluída. Reinicie o serviço com:"
 echo "    systemctl restart bind9"
