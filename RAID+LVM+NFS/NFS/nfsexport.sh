a76905af143d0f297ced549e3336b7d7b93f37e3
Marlon Severo <marlonsallessevero@hotmail.com>
2025-06-22 13:58:17 -0300

    NFS config

---
+++ RAID+LVM+NFS/NFS/nfsexport.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in drivers infrastructure lixeiras manager marketing owner profiles public security devel homes;
+do echo -e "/srv/asf/samba/$i  \t\t192.168.1.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; 
+done
